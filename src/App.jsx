f0eff65b79ef9a3f5144df3fede6b876b9835b96
Vladpers1k <vladik340123@gmail.com>
2024-12-31 12:22:26 +0200

    forget to add prettier also added some information in readme and fixed error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
-import React, { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import axios from "axios";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import axios from 'axios'
+import './App.css'
 
 const DataFetcher = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        setLoading(true); // Початок завантаження
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setData(response.data);
+        setLoading(true)
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+        setData(response.data)
       } catch (err) {
-        setError("Сталася помилка під час завантаження даних.");
+        setError('Сталася помилка під час завантаження даних.')
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   if (loading) {
-    return <p>Завантаження даних...</p>;
+    return <p>Завантаження даних...</p>
   }
 
   if (error) {
-    return <p>{error}</p>;
+    return <p>{error}</p>
   }
 
   return (
@@ -47,7 +45,7 @@ const DataFetcher = () => {
           ))}
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default DataFetcher;
+export default DataFetcher
